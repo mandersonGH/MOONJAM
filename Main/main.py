702e9e78e6d7122bc8646ff155d246c43e12f901
Miguel Anderson <mra21@duke.edu>
2017-09-09 00:01:03 -0400

    last touches2

--- Main/main.py
+++ Main/main.py
@@ -9,9 +9,9 @@ Created on Sep 8, 2017
 
 
 def main():
-    #controller = Controller(sys.argv)
-    inputs = ['', r'C:\Users\Mande\Desktop\E + A Directory' + "\\", 'mpl4','pipe3d', 'requested','elines','lum_fracs','indices.cs']
-    controller = Controller(inputs)
+    controller = Controller(sys.argv)
+    # inputs = ['', r'C:\Users\Mande\Desktop\E + A Directory' + "\\", 'mpl4','pipe3d', 'requested','elines','lum_fracs','indices.cs']
+    # controller = Controller(inputs)
     controller.run()
 
 
